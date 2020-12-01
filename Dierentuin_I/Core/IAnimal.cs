01ad05b04bd9373362f06b47db376c45e055c05d
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2020-12-01 13:06:32 +0100

    stap 3

--- Dierentuin_I/Core/IAnimal.cs
+++ Dierentuin_I/Core/IAnimal.cs
@@ -9,4 +9,5 @@ namespace Dierentuin_I.Core
     {
        
     }
+
 }
--- Dierentuin_I/MainWindow.xaml.cs
+++ Dierentuin_I/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Dierentuin_I.Core;
 using Dierentuin_I.Models;
+using RandomNameGeneratorLibrary;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -25,6 +26,8 @@ namespace Dierentuin_I
     public partial class MainWindow : Window
     {
         readonly List<Animal> AllAnimals = new List<Animal>();
+        public int DeadAnimals = 0;
+
 
         public MainWindow()
         {
@@ -33,65 +36,123 @@ namespace Dierentuin_I
             timer.Interval = TimeSpan.FromMilliseconds(500);
             timer.Tick += timer_tick;
             timer.Start();
-            CB_animals.Items.Add("Aap");
-            CB_animals.Items.Add("Leeuw");
-            CB_animals.Items.Add("Olifant");
 
         }
         void timer_tick(object sender, EventArgs e)
         {
             foreach (var animal in AllAnimals)
             {
-                animal.Energy = animal.EnergyUse();          
+                animal.Energy = animal.EnergyUse();                
             }
             update();
         }
 
-        private void AddNewAnimal_Click(object sender, RoutedEventArgs e)
+        //private void AddNewAnimal_Click(object sender, RoutedEventArgs e)
+        //{
+          
+
+        //    if (CB_animals.SelectedIndex != -1)
+        //    {
+        //        switch(CB_animals.SelectedIndex)
+        //        {
+        //            case 0:
+        //                Animal monkey = new Monkey
+        //                {
+        //                    TypeOfAnimal = CB_animals.SelectedItem.ToString(),
+        //                    Name = name,
+        //                    Energy = 60
+        //                };
+        //                AllAnimals.Add(monkey);
+        //                break;
+        //            case 1:
+        //                Animal lion = new Lion
+        //                {
+        //                    TypeOfAnimal = CB_animals.SelectedItem.ToString(),
+        //                    Name = name,
+        //                    Energy = 100
+        //                };
+        //                AllAnimals.Add(lion);
+        //                break;
+        //            case 2:
+        //                Animal elephant = new Elephant
+        //                {
+        //                    TypeOfAnimal = CB_animals.SelectedItem.ToString(),
+        //                    Name = name,
+        //                    Energy = 100
+        //                };
+        //                AllAnimals.Add(elephant);
+        //                break;
+        //        }
+        //        update();
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("U heeft nog geen naam en/of dier geselecteerd");
+        //    }
+        //}
+
+        public void update()
         {
-            if (CB_animals.SelectedIndex != -1 && !string.IsNullOrEmpty(TB_NameAnimal.Text))
+            var Monkeys = new List<Animal>();
+            var Elephants = new List<Animal>();
+            var Lions = new List<Animal>();
+            var FilteredAnimals = new List<Animal>();
+
+            if (cb_monkeys.IsChecked == true)
             {
-                switch(CB_animals.SelectedIndex)
-                {
-                    case 0:
-                        Animal monkey = new Monkey
-                        {
-                            TypeOfAnimal = CB_animals.SelectedItem.ToString(),
-                            Name = TB_NameAnimal.Text,
-                            Energy = 60
-                        };
-                        AllAnimals.Add(monkey);
-                        break;
-                    case 1:
-                        Animal lion = new Lion
-                        {
-                            TypeOfAnimal = CB_animals.SelectedItem.ToString(),
-                            Name = TB_NameAnimal.Text,
-                            Energy = 100
-                        };
-                        AllAnimals.Add(lion);
-                        break;
-                    case 2:
-                        Animal elephant = new Elephant
-                        {
-                            TypeOfAnimal = CB_animals.SelectedItem.ToString(),
-                            Name = TB_NameAnimal.Text,
-                            Energy = 100
-                        };
-                        AllAnimals.Add(elephant);
-                        break;
-                }
-                update();
+                Monkeys = AllAnimals.Where(a => a.TypeOfAnimal == "Aap").ToList();
+                FilteredAnimals = Monkeys.Concat(Lions.Concat(Elephants)).ToList();
+            }
+            
+            if (cb_Lions.IsChecked == true)
+            { 
+                Lions = AllAnimals.Where(a => a.TypeOfAnimal == "Leeuw").ToList();
+                FilteredAnimals = Monkeys.Concat(Lions.Concat(Elephants)).ToList();
+            }
+
+            if (cb_elephants.IsChecked == true)
+            {
+                 Elephants = AllAnimals.Where(a => a.TypeOfAnimal == "Olifant").ToList();
+                FilteredAnimals = Monkeys.Concat(Lions.Concat(Elephants)).ToList();
+            }
+
+            if (cb_elephants.IsChecked == false && cb_monkeys.IsChecked == false && cb_Lions.IsChecked == false)
+            {
+                DG_Animals.ItemsSource = AllAnimals.ToList();
             }
             else
             {
-                MessageBox.Show("U heeft nog geen naam en/of dier geselecteerd");
+                DG_Animals.ItemsSource = FilteredAnimals.ToList();
             }
-        }
 
-        public void update()
-        {         
-            DG_Animals.ItemsSource = AllAnimals.ToList();
+            //var FilteredAnimals = new List<Animal>();
+
+            //if (cb_monkeys.IsChecked == true)
+            //{
+            //    DG_Animals.ItemsSource = FilteredAnimals.Concat(AllAnimals.Where(a => a.TypeOfAnimal == "Aap"));
+            //}
+            //else if (cb_Lions.IsChecked == true)
+            //{ 
+            //    DG_Animals.ItemsSource = FilteredAnimals.Concat(AllAnimals.Where(a => a.TypeOfAnimal == "Leeuw"));
+            //}
+            //else if (cb_elephants.IsChecked == true)
+            //{
+            //    DG_Animals.ItemsSource = FilteredAnimals.Concat(AllAnimals.Where(a => a.TypeOfAnimal == "Olifant"));
+            //}
+            //else
+            //{
+            //    DG_Animals.ItemsSource = AllAnimals.ToList();
+            //}
+            foreach (var animal in AllAnimals.ToList())
+            {
+                if (animal.Energy <= 0)
+                {
+                    AllAnimals.Remove(animal);
+                    DeadAnimals += 1;
+                    lb_deadanimals.Content = "Aantal dode dieren: " + DeadAnimals.ToString();
+                }              
+            }
+
         }
 
         private void FeedAllAnimals_Click(object sender, RoutedEventArgs e)
@@ -126,7 +187,6 @@ namespace Dierentuin_I
             }
             update();
         }
-
         private void FeedElephant_Click(object sender, RoutedEventArgs e)
         {
             foreach (var animal in AllAnimals)
@@ -137,6 +197,45 @@ namespace Dierentuin_I
                 }
             }
             update();
+        }
+
+        private void Btn_Add_Monkey_Click(object sender, RoutedEventArgs e)
+        {
+            var _personGenerator = new PersonNameGenerator();
+            var name = _personGenerator.GenerateRandomFirstAndLastName();
+            var monkey = new Monkey
+            {
+                TypeOfAnimal ="Aap",
+                Name = name,
+                Energy = 60
+            };
+            AllAnimals.Add(monkey);
+        }
+
+        private void Btn_Add_Lion_Click(object sender, RoutedEventArgs e)
+        {
+            var _personGenerator = new PersonNameGenerator();
+            var name = _personGenerator.GenerateRandomFirstAndLastName();
+            var lion = new Lion
+            {
+                TypeOfAnimal = "Leeuw",
+                Name = name,
+                Energy = 100
+            };
+            AllAnimals.Add(lion);
+        }
+
+        private void Btn_Add_Elephant_Click(object sender, RoutedEventArgs e)
+        {
+            var _personGenerator = new PersonNameGenerator();
+            var name = _personGenerator.GenerateRandomFirstAndLastName();
+            var elephant = new Elephant
+            {
+                TypeOfAnimal = "Olifant",
+                Name = name,
+                Energy = 100
+            };
+            AllAnimals.Add(elephant);
         }
     }
 }
